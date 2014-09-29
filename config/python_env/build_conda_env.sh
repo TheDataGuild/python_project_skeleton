2755605a2b34ed9680396ad9f689c99d6798226a
Chris Diehl <chris@thedataguild.com>
2014-09-29 12:14:09 -0700

    Simplifies the Python environment build process

--- config/python_env/build_conda_env.sh
+++ config/python_env/build_conda_env.sh
@@ -1,4 +1,4 @@
 # Create a DataGuild conda environment based on the conda and pip specs
-conda create --name DataGuild --file conda.spec
+conda create --yes --name DataGuild anaconda=2.1.0
 source activate DataGuild
 pip install -r pip.spec
